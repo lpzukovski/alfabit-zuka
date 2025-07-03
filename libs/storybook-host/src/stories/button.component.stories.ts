7c237a824f03cda139b003a5d0180f81ebecc950
Lucas Paulo Zukovski <zukovski@gmail.com>
2025-07-03 10:37:02 -0300

    Primeiro commit

---
+++ libs/storybook-host/src/stories/button.component.stories.ts
@@ -0,0 +1,86 @@
+import type {Meta, StoryObj} from "@storybook/angular"
+import {ButtonComponent} from '@alfabit-zuka/button';
+
+
+const meta: Meta<ButtonComponent> = {
+    component: ButtonComponent
+}
+
+export default meta;
+
+type Story = StoryObj<ButtonComponent>;
+
+export const PrimaryButton: Story = {
+    args: {
+        text: 'Action', 
+        variant: 'primary',
+        disabled: false,
+        theme: 'blue'
+    }
+};
+export const PrimaryButtonDisabled: Story = {
+    args: {
+        ...PrimaryButton.args,
+        disabled: true
+    }
+};
+export const SecondaryButton: Story = {
+    args: {
+        ...PrimaryButton.args,
+        variant: 'secondary'
+    }
+};
+export const SecondaryButtonDisabled: Story = {
+    args: {
+        ...PrimaryButtonDisabled.args,
+        variant: 'secondary'
+    }
+};
+export const TertiaryButton: Story = {
+    args: {
+        ...PrimaryButton.args,
+        variant: 'tertiary'
+    }
+};
+export const TertiaryButtonDisabled: Story = {
+    args: {
+        ...PrimaryButtonDisabled.args,
+        variant: 'tertiary'
+    }
+};
+export const PrimaryButtonViolet: Story = {
+    args: {
+        ...PrimaryButton.args,
+        theme: 'violet'
+    }
+};
+export const PrimaryButtonDisabledViolet: Story = {
+    args: {
+        ...PrimaryButtonDisabled.args,
+        theme:'violet'
+    }
+};
+export const SecondaryButtonViolet: Story = {
+    args: {
+        ...SecondaryButton.args,
+        theme:'violet'
+    }
+};
+export const SecondaryButtonDisabledViolet: Story = {
+    args: {
+        ...SecondaryButtonDisabled.args,
+        theme:'violet'
+    }
+};
+export const TertiaryButtonViolet: Story = {
+    args: {
+        ...TertiaryButton.args,
+        theme:'violet'
+    }
+};
+export const TertiaryButtonDisabledViolet: Story = {
+    args: {
+        ...TertiaryButtonDisabled.args,
+        theme:'violet'
+    }
+};
