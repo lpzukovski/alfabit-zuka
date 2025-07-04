2d1be079c75e2a4fed66a5dc35d9c511ed5dccd1
Lucas Paulo Zukovski <zukovski@gmail.com>
2025-07-04 10:41:24 -0300

    feat(typography): renderiza componente dinamicamente com `ngComponentOutlet`

--- libs/ui/typography/src/lib/typography/typography.component.ts
+++ libs/ui/typography/src/lib/typography/typography.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 type Text =
@@ -9,6 +9,46 @@ type Text =
   | 'subtitle2'
   | 'normal';
 
+type Size =
+  | 'title1'
+  | 'title2'
+  | 'title3'
+  | 'subtitle1'
+  | 'subtitle2'
+  | 'lg'
+  | 'md'
+  | 'sm'
+  | 'xs';
+
+@Component({
+  standalone: true,
+  styleUrl: './typography.component.css',
+  template: `
+    <h1 class="typography {{ size }}">
+      <ng-content/>
+    </h1>
+  `,
+})
+export class H1Component {
+    @Input()
+    size!: Size;
+}
+
+
+@Component({
+  standalone: true,
+  styleUrl: './typography.component.css',
+  template: `
+  <span class="typography {{ size }}">
+    <ng-content/>
+  </span>
+  `,
+})
+export class SpanComponent {
+    @Input()
+    size!: Size;
+}
+
 @Component({
   selector: 'ab-typography',
   standalone: true,
@@ -16,7 +56,41 @@ type Text =
   templateUrl: './typography.component.html',
   styleUrl: './typography.component.css',
 })
-export class TypographyComponent {
+export class TypographyComponent implements OnInit {
   @Input()
   variant: Text = 'normal';
+
+  @Input()
+  size!: Size;
+
+  @ViewChild('template', {static: true})
+  template!: TemplateRef<unknown>;
+
+  component: any = SpanComponent;
+
+  dynamicComponentContent!: any[][];
+
+  get inputs(){
+    return {size: this.size}
+  }
+
+  private componentsMap = {
+    title1: H1Component,
+    title2: SpanComponent,
+    title3: SpanComponent,
+    subtitle1: SpanComponent,
+    subtitle2: SpanComponent,
+    normal: SpanComponent,
+  } satisfies {[key in Text]: any};
+
+  constructor(private viewContainerRef: ViewContainerRef){}
+
+  ngOnInit(): void {
+    this.component = this.componentsMap[this.variant];
+
+    const templateContent = this.viewContainerRef.createEmbeddedView(this.template).rootNodes;
+    
+    this.dynamicComponentContent = [templateContent];
+  }
+
 }
