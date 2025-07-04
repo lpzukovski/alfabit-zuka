af57f4ddc54193f8ce6cb7a5f11ae96f4f207625
Lucas Paulo Zukovski <zukovski@gmail.com>
2025-07-04 11:52:35 -0300

    feat(storybook-host): adiciona stories restantes ao typography

--- libs/storybook-host/src/lib/Atoms/typography.component.stories.ts
+++ libs/storybook-host/src/lib/Atoms/typography.component.stories.ts
@@ -7,7 +7,7 @@ const meta: Meta<TypographyComponent> = {
     props: args,
     template: `
       <ab-typography ${argsToTemplate(args)}>
-        Text
+        Novo Conteúdo
       </ab-typography>
     `
   })
@@ -15,8 +15,67 @@ const meta: Meta<TypographyComponent> = {
 export default meta;
 type Story = StoryObj<TypographyComponent>;
 
-export const Primary: Story = {
+export const Title1: Story = {
   args: {
     variant: 'title1'
   },
 };
+export const Title2: Story = {
+  args: {
+    variant: 'title2'
+  },
+};
+export const Title3: Story = {
+  args: {
+    variant: 'title3'
+  },
+};
+export const Subtitle1: Story = {
+  args: {
+    variant: 'subtitle1'
+  },
+};
+export const Subtitle2: Story = {
+  args: {
+    variant: 'subtitle2'
+  },
+};
+export const TextLarge: Story = {
+  args: {
+    variant: 'normal'
+  },
+};
+export const TextMedium: Story = {
+  args: {
+    ...TextLarge.args,
+    size: 'md'
+  },
+};
+export const TextSmall: Story = {
+  args: {
+    ...TextLarge.args,
+    size: 'sm'
+  },
+};
+export const TextExtraSmall: Story = {
+  args: {
+    ...TextLarge.args,
+    size: 'xs'
+  },
+};
+export const Paragraph: Story = {
+  args: {
+    ...TextLarge.args
+  },
+ render: (args)=> ({
+    props: args,
+    template: `
+      <p>
+        <ab-typography ${argsToTemplate(args)}>
+          Novo Conteúdo
+        </ab-typography>
+      </p>
+    `
+  })
+
+}
